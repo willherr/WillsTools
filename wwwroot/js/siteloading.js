f7de0198531908ee2514aebf2e95925c8fc8fe26
William Herrmann <herrmanw@mail.gvsu.edu>
2020-03-01 19:42:10 -0500

    split up js. better bottom's up parsing. versioning for page refresh

---
+++ wwwroot/js/siteloading.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    "use strict";
+    $("#LOADING").text("Loading Will's Tools");
+    let loadingInterval = setInterval(function () {
+        const $loading = $("#LOADING");
+        if ($loading.length) {
+            $("#LOADING").text($("#LOADING").text() + ".");
+        } else {
+            clearInterval(loadingInterval);
+        }
+    }, 250);
+
+    setTimeout(function () {
+        const $loading = $("#LOADING");
+        if ($loading.length) {
+            clearInterval(loadingInterval);
+            $loading.text("This is taking too long. Ask Will why.");
+            if (/iPad|iPhone|iPod/.test(navigator.platform) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) {
+                $loading.parent().addClass("p-3").append("Note: IOS is not supported yet. I'm developing this site with pre-released Microsoft software called Blazor Web Assembly. The official release date is in May, so you may have to wait until then. I know, the anticipation is probably killing you.");
+            }
+        }
+    }, 10000)
+})()
---
+++ wwwroot/js/utility.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+window.Utility = {
+    // utility functions
+    scrollToBottom: function () {
+        window.scrollTo(0, document.body.scrollHeight);
+    },
+
+    // jquery wrappers
+    addClass: function (targetSelector, cssClass) {
+        $(targetSelector).addClass(cssClass);
+    },
+    removeClass: function (targetSelector, cssClass) {
+        $(targetSelector).removeClass(cssClass);
+    }
+};
