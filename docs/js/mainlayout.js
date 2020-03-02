705520ec017558bac0c59705c31d4b59059557b2
William Herrmann <herrmanw@mail.gvsu.edu>
2020-03-01 19:45:05 -0500

    publish

---
+++ docs/js/mainlayout.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+window.MainLayout = {
+    initialize: function () {
+        $('.dropdown').on('hide.bs.dropdown', function (e) {
+            if (e.clickEvent && e.clickEvent.target) {
+                if ($(e.clickEvent.target).is("button") && $(e.clickEvent.target).parent(".dropdown-menu").length) {
+                    return false;
+                }
+            }
+            return true;
+        });
+    }
+};
