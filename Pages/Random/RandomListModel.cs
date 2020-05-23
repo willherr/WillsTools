9e6493a3c804ea35d060baa75ad3ca8e6d0452f3
William Herrmann <herrmanw@mail.gvsu.edu>
2020-05-22 21:15:04 -0400

    randomize list

---
+++ Pages/Random/RandomListModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SysRandom = System.Random;
+
+namespace WillsToolsWasm.Pages.Random
+{
+    public class RandomListModel
+    {
+        private static readonly SysRandom Random = new SysRandom();
+        private Func<Task<string>> getRandomWord;
+        private Action stateHasChanged;
+
+        public List<string> Items { get; set; } = new List<string>();
+        public string Input { get; set; }
+        public string RandomItem { get; set; }
+
+        public RandomListModel(Func<Task<string>> getRandomWord, Action stateHasChanged)
+        {
+            this.getRandomWord = getRandomWord;
+            this.stateHasChanged = stateHasChanged;
+        }
+
+        public void AddInputToList()
+        {
+            Items.Add(Input);
+            Input = null;
+        }
+
+        public void Randomize()
+        {
+            if (Items.Count == 0)
+            {
+                getRandomWord().ContinueWith(randomWord =>
+                {
+                    RandomItem = randomWord.Result;
+                    stateHasChanged();
+                });
+            }
+            else
+            {
+                RandomItem = Items[Random.Next(Items.Count)];
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace WillsToolsWasm
 {
@@ -9,6 +16,7 @@ namespace WillsToolsWasm
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
